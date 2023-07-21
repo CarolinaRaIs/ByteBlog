e6b5f2fa2ad82fba3e2f9bdd0bc6ca8f2e4e20f5
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-07-20 18:08:44 -0700

    Added logout route to user-routes.js in controllers folder

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const { User, Post, Comment } = require('../../models');
+const bcrypt = require('bcrypt');
+
+// Get all users with associated posts and comments
+router.get('/', async (req, res) => {
+    try {
+        const dbUsers = await User.findAll({ include: [Post, Comment] });
+        res.json(dbUsers);
+    } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: 'An error occurred', err });
+  }
+});
+
+// Logout route
+router.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ message: 'Failed to log out' });
+        }
+        res.clearCookie('session-id')
+        res.redirect('/');
+    });
+});
+
+// Get a specific user by ID with associated posts and comments
+router.get('/:id', async (req, res) => {
+    try {
+        const dbUser = await User.findByPk(req.params.id, { include: [Post, Comment] });
+        res.json(dbUser);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: 'An error occurred', err });
+    }
+});
+
