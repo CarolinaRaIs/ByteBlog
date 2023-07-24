58d173302da9df78f9a251964204b5ba83512fe2
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-07-23 22:57:32 -0700

    Created a new blog post route in post-routes.js in controllers folder

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -7,3 +7,26 @@ const { User, Post, Comment } = require('../../models');
 // This middleware is used to check if a user is authenticated before allowing access to certain routes
 // It is used as a middleware in routes that require authentication
 const withAuth = require('../../utils/auth');
+
+// Create a new blog post
+router.post('/', async (req, res) => {
+    try {
+        // Extract the 'title' and 'content' from the request body
+        const { title, content } = req.body;
+
+        // Create a new post in the database with the provided 'title', 'content', and 'user_id' from the session
+        const newPost = await Post.create({
+            title,
+            content,
+            user_id: req.session.user.id,
+        });
+
+        // Send the newly created post as a JSON response
+        res.json(newPost);
+
+        } catch (err) {
+            // If an error occurs during the database query or response, handle the error
+            console.error(err);
+            res.status(500).json({ msg: 'An error occurred', err });
+        }
+    });
