c527e2e5ce349645af391969f7d0f9b11daa0573
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-07-24 08:19:47 -0700

    Added create, update and delete commment routes in comment-routes.js within controllers -api folder

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -33,3 +33,72 @@ router.get('/:id', async (req, res) => {
       res.status(500).json({ msg: 'An error occurred', err });
     }
 });
+
+// Create a new comment
+router.post('/', withAuth, async (req, res) => {
+    try {
+        // Extract the 'comment_text', 'user_id', and 'post_id' from the request body
+        const { comment_text, post_id } = req.body;
+  
+        // Create a new comment in the database with the provided 'comment_text', 'user_id', and 'post_id'
+        const newComment = await Comment.create({
+            comment_text,
+            user_id: req.session.user.id,
+            post_id,
+        }, 
+        {
+            include: [User, Post]
+        });
+  
+        // Send the newly created comment as a JSON response
+        res.json(newComment);
+
+    } catch (err) {
+        // If an error occurs during the database query or response, handle the error
+        console.log(err);
+        res.status(500).json({ msg: 'An error occurred', err });
+    }
+});
+  
+// Update a comment by ID
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        // Update the comment with the provided ID in the database with the data from the request body
+        const updatedComment = await Comment.update(req.body, {
+            where: {
+                id: req.params.id
+        }
+      });
+  
+      // Send the updated comment as a JSON response
+      res.json(updatedComment);
+
+    } catch (err) {
+      // If an error occurs during the database query or response, handle the error
+      console.log(err);
+      res.status(500).json({ msg: 'An error occurred', err });
+    }
+});
+
+// Delete a comment by ID
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        // Delete the comment with the provided ID from the database
+        const delComment = await Comment.destroy({
+            where: {
+                id: req.params.id
+        }
+      });
+  
+        // Send the deleted comment as a JSON response
+        res.json(delComment);
+    
+    } catch (err) {
+        // If an error occurs during the database query or response, handle the error
+        console.log(err);
+        res.status(500).json({ msg: 'An error occurred', err });
+    }
+});
+  
+module.exports = router;
+  
