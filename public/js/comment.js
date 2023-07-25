45a9ce91bac0cd74e0d2a6711bb3b878d18bbe3c
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-07-25 10:55:07 -0700

    Added code to dashboard.js

--- public/js/comment.js
+++ public/js/comment.js
@@ -3,40 +3,40 @@ const commentInput = document.querySelector("#comment");
 const commentIdInput = document.querySelector("#CommentId");
 
 document.querySelector("#newComment").addEventListener("submit", event => {
-  //The default action of a form submit event is to send the form data to the URL specified in the form's action attribute and then refresh the page.
-  //Handling the form submission with JS and sending form data using the Fetch API. 
-  //Thats why don't want the page to refresh, it would interrupt the JS code (therefore not handle the form data.).
-  event.preventDefault();
+    //The default action of a form submit event is to send the form data to the URL specified in the form's action attribute and then refresh the page.
+    //Handling the form submission with JS and sending form data using the Fetch API. 
+    //Thats why don't want the page to refresh, it would interrupt the JS code (therefore not handle the form data.).
+    event.preventDefault();
   
-  // Destructure values from form elements
-  const comment_text = commentInput.value;
-  const post_id = commentIdInput.value;
+    // Destructure values from form elements
+    const comment_text = commentInput.value;
+    const post_id = commentIdInput.value;
 
-  // Create new comment object
-  const newComment = { comment_text, post_id };
+    // Create new comment object
+    const newComment = { comment_text, post_id };
 
-  // Post new comment
-  fetch("/api/comments", {
-    method: "POST",
-    body: JSON.stringify(newComment),
-    headers: { "Content-Type": "application/json" }
-  })
+    // Post new comment
+    fetch("/api/comments", {
+        method: "POST",
+        body: JSON.stringify(newComment),
+        headers: { "Content-Type": "application/json" }
+    })
 
-  // Post new comment
-  fetch("/api/comments", {
-    method: "POST",
-    body: JSON.stringify(newComment),
-    headers: { "Content-Type": "application/json" }
-  })
+    // Post new comment
+    fetch("/api/comments", {
+        method: "POST",
+        body: JSON.stringify(newComment),
+        headers: { "Content-Type": "application/json" }
+    })
 
-  .then(response => {
-    if (response.ok) {
-        console.log("Comment posted:", newComment);
-        location.reload();
-    } else {
-        alert("There was an error, please try again.");
-    }
-  })
-  .catch(error => console.error('Error:', error));
+    .then(response => {
+        if (response.ok) {
+            console.log("Comment posted:", newComment);
+            location.reload();
+        } else {
+            alert("There was an error, please try again.");
+        }
+    })
+    .catch(error => console.error('Error:', error));
 
 });
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,10 +1,68 @@
-// Element selectors
-var postsSection = document.querySelector("#existingposts");
-var createPostSection = document.querySelector("#createNew");
-var addPostBtn = document.querySelector("#addNewPost");
-var newPost = document.querySelector("#newPost");
+// Get DOM elements
+const postsSection = document.querySelector("#existingposts");
+const createPostSection = document.querySelector("#createNew");
+const addPostBtn = document.querySelector("#addNewPost");
+const newPost = document.querySelector("#newPost");
+//const titleInput = document.querySelector("#title");
+//const contentInput = document.querySelector("#content");
 
-function hideCreatePostSection() {
-    createPostSection.hidden = true; 
-}
+// Hide the "Create New Post" section initially
+const hideCreateNewPost = () => {
+    newPostContainer.style.display = "none";
+};
 
+hideCreateNewPost();
+
+// Event listener for the "Add New Post" button
+addNewPostButton.addEventListener("click", event => {
+    event.preventDefault();
+    console.log('click');
+    existingPosts.hidden = true;
+    addNewPostButton.hidden = true;
+    createNew.style.hidden = false;
+});
+
+// Event listener for the "New Post" form submission
+postForm.addEventListener("submit", async event => {
+    event.preventDefault();
+    // Get the values from the input fields
+    const title = titleInput.value;
+    const content = contentInput.value;
+  
+    // Check if both title and content are provided
+    if (!title || !content) {
+        alert("Please enter a title and some content");
+        return;
+    }
+
+    // Create a new post object with the title and content
+    const newPost = {
+        title: title,
+        content: content
+    };
+
+    console.log(newPost);
+
+    try {
+        // Send a POST request to the server to create a new post
+        const response = await fetch("/api/posts", {
+            method: "POST", 
+            body: JSON.stringify(newPost),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (response.ok) {
+            // If the response is successful, hide the "Create New Post" section and reload the page
+            createNew.setAttribute("hidden", "true");
+            location.reload();
+          } else {
+            // If there's an error, show an alert
+            alert("Error - please try again");
+          }
+        } catch (error) {
+          // If an exception occurs during the fetch, show an alert with the error message
+          alert("Error - " + error.message);
+        }
+});
